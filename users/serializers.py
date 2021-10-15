101c0dd2b7326b31217579be86a0a383d5bb5a87
Ali Arain <deistali@outlook.com>
2021-10-15 09:34:00 +0500

    Getting User Detail And Custom User Detail Added👌

--- users/serializers.py
+++ users/serializers.py
@@ -9,3 +9,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email','first_name','last_name', 'avatar', 'superhost')
+
+class WriteUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields =(
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'avatar',
+        )
--- users/views.py
+++ users/views.py
@@ -1,18 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
-from .serializers import UserSerializer
+from .serializers import UserSerializer , WriteUserSerializer
+from .models import User
 
 
 class MeView(APIView):
+    permission_classes = [IsAuthenticated,]
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(UserSerializer(request.user).data)
+        return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        pass
+        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
 
 
 @api_view(["GET"])
-class user_detail():
-    pass
+def user_detail(request, pk ):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=404)
